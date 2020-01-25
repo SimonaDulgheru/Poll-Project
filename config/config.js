915c5fcd48e85024890adcf83203b3775a5c644c
Simona <s.c.dulgheru@gmail.com>
2020-01-25 11:08:52 +0000

    config changes.

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports ={
       "username": "root",
       "password": process.env.DEV_PASSWORD,
       "database": "pollApp_dev",
-      "host": "127.0.0.1",
+      "host": "mysql://b2bfa872bbbe6f:cbc74f59@us-cdbr-iron-east-05.cleardb.net/heroku_b7872ff6bb385b5?reconnect=true",
       "dialect": "mysql"
     },
     "test": {
